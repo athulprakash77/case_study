82a45b7d283cacbcb84cd06177a4deb161bf55be
athulprakash007 <athull362@gmail.com>
2022-06-25 13:10:34 -0700

    updating new file

---
+++ contact.js
@@ -0,0 +1,15 @@
+contactbtnevent=()=>
+{
+     Name=(document.getElementById("name").value)
+     message=(document.getElementById("message").value)
+     phone=(document.getElementById("phone").value)
+
+     console.log(Name)
+alert(Name)
+
+console.log(message)
+alert(message)
+
+console.log(phone)
+alert(phone)
+}
